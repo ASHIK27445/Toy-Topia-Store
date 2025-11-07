12a3f59dcb8b6172fb062a14a52bb12ccba8a565
ASHIK27445 <mdashikulislama57@gmail.com>
2025-11-07 09:59:36 +0600

    create observer for current user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,8 +1,11 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from "firebase/auth"
 import { AuthContext } from "./AuthContext"
 import { auth } from "../firebase/firebase.init"
+import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
     const createUserEP = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
@@ -13,9 +16,21 @@ const AuthProvider = ({children}) => {
         }
         return updateProfile(auth.currentUser, profile)
     }
+    const loginUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged(auth, (user)=>{
+            setUser(user)
+            toast('state change')
+        })
+
+        return () => unsubscribe()
+    }, [])
     const authInfo = {
         createUserEP,
-        profileUpdate
+        profileUpdate,
+        loginUser
     }
     return(
         <AuthContext value={authInfo}>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,26 +3,17 @@ import { AuthContext } from '../context/AuthContext';
 import { toast } from 'react-toastify';
 import { NavLink } from 'react-router';
 const Login = () => {
-    const {createUserEP, profileUpdate} = use(AuthContext)
+    const {loginUser} = use(AuthContext)
     const handleLogin = (e) =>{
         e.preventDefault()
         
-        const name = e.target.name.value
-        const photoURL = e.target.photoURL.value
         const email = e.target.email.value
         const password = e.target.password.value
-        console.log(name, photoURL, email, password)
-        createUserEP(email, password)
-            .then(res=> {
-                console.log(res.user)
-                profileUpdate(name, photoURL)
-                    .then(()=> {})
-                    .catch(error => console.log(error.mesaage))
-                toast("Create User Successfully!", {
-                    autoClose: 1200
-                })
-            })
-            .catch(e=> console.log(e.mesaage))
+        console.log(email, password)
+        loginUser(email, password)
+            .then(res => console.log(res.user))
+            .catch(e=> console.log(e.message))
+
     }
   return (
     <div className="w-11/12 mx-auto">
@@ -33,10 +24,6 @@ const Login = () => {
               <h1 className="text-4xl font-bold mt-4">Login now!</h1>
                 <form onSubmit={handleLogin}>
                     <fieldset className="fieldset">
-                        <label className="label text-blue-800 font-semibold">Name</label>
-                        <input type="text" className="input input-ghost border-b-blue-800 mb-2" placeholder="Name" name="name" required />
-                        <label className="label text-blue-800 font-semibold">PhotoURL</label>
-                        <input type="text" className="input input-ghost border-b-blue-800 mb-2" placeholder="photoURL" name='photoURL' required />
                         <label className="label text-blue-800 font-semibold">Email</label>
                         <input type="email" className="input input-ghost border-b-blue-800 mb-2" placeholder="Email" name='email' required />
                         <label className="label text-blue-800 font-semibold">Password</label>
@@ -47,8 +34,8 @@ const Login = () => {
                         name='password'
                         required
                         />
-                        <div>Already have an account? 
-                        <NavLink to="/login" className="link link-hover text-blue-700 font-semibold"> Login here!</NavLink>
+                        <div>New user? 
+                        <NavLink to="/register" className="link link-hover text-blue-700 font-semibold"> Register here!</NavLink>
                         </div>
                         <button className="btn btn-neutral mt-4 mb-4">Login</button>
                     </fieldset>
