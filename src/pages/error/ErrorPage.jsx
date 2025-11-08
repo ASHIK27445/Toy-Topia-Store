84017e2c76df385e82af3bed841bb8c79d4b31ee
ASHIK27445 <mdashikulislama57@gmail.com>
2025-11-08 13:18:38 +0600

    Added footer and navbar in the error page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,18 +1,32 @@
 import FuzzyText from '../../components/ReactBlitz/FuzzyText'
+import Top from '../../components/header/Top';
+import Navbar from '../../components/header/Navbar';
+import Footer from '../../components/Footer/Footer';
 const ErrorPage = () => {
     const enableHover = true
     const hoverIntensity = 0.5
   return (
-    <div className='flex flex-col gap-5 justify-center items-center bg-black min-h-screen'>
-      <FuzzyText
-        baseIntensity={0.2}
-        hoverIntensity={hoverIntensity}
-        enableHover={enableHover}>
-        404
-      </FuzzyText>
-      <FuzzyText fontSize="2rem" baseIntensity={0.1}>
-        Oops! Not Found
-      </FuzzyText>
+    <div>
+        <header>
+            <Top></Top>
+            <nav>
+                <Navbar></Navbar>
+            </nav>
+        </header>
+        <div className='flex flex-col gap-5 justify-center items-center bg-black min-h-screen'>
+          <FuzzyText
+            baseIntensity={0.2}
+            hoverIntensity={hoverIntensity}
+            enableHover={enableHover}>
+            404
+          </FuzzyText>
+          <FuzzyText fontSize="2rem" baseIntensity={0.1}>
+            Oops! Not Found
+          </FuzzyText>
+        </div>
+        <footer>
+            <Footer></Footer>
+        </footer>
     </div>
   );
 };
