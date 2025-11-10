00f9ed5a0736496c9dc9987b9a20aff952009494
ASHIK27445 <mdashikulislama57@gmail.com>
2025-11-10 18:13:59 +0600

    Adding responsive design in toy details page

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -24,9 +24,9 @@ const AllToys = () => {
 
     return(
         <div className="w-11/12 mx-auto mt-16">
-            <main className="flex justify-center gap-10">
+            <main className="flex flex-col md:flex-row justify-center gap-10">
                 <div>
-                    <div className="border border-gray-900 border-dashed rounded-2xl px-1 py-1.5">
+                    <div className="border border-gray-900 border-dashed rounded-2xl px-2 py-2.5 sticky top-4">
                         <h2 className="font-comfortaa font-extrabold text-lg">Product Categories</h2>
                         <p onClick={()=> handleSelectCategory(null)}
                             className={`py-1.5 cursor-pointer ${!selectedCategories? 'text-blue-700 font-semibold' : 'hover:text-blue-700 hover:font-semibold'}`}
@@ -44,9 +44,9 @@ const AllToys = () => {
                         }
                     </div>
                 </div>
-                <div className="w-8/12">
+                <div className="w-11/12 lg:w-8/12 mx-auto">
                     <h2 className="font-extrabold text-[42px] mb-3 font-comfortaa">{selectedCategories || 'All Toys'}</h2>
-                    <div className="grid grid-cols-3 gap-5 mb-8">
+                    <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 mb-8">
                     {
                         filtredToyCard
                         .map(toys=> <ToyCard key={toys.toyId} toys={toys}></ToyCard>)
